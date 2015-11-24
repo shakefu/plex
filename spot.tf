08ce9f40fde7db566fbe6be333ae17f309cd5719
Jacob Alheid <shakefu@gmail.com>
2015-11-23 21:35:01 -0800

    Twiddle.

--- spot.tf
+++ spot.tf
@@ -3,7 +3,7 @@ resource "aws_spot_instance_request" "plex-spot" {
     ami = "ami-73bed013"
     availability_zone = "us-west-1b"
     instance_type = "m2.xlarge"
-    security_groups = [ "${aws_security_group.allow_all.id}" ]
+    # security_groups = [ "${aws_security_group.allow_all.id}" ]
     key_name = "plex-spot"
 
     # user_data = "${file("plex.yml")}"
@@ -36,4 +36,9 @@ resource "aws_security_group" "allow_all" {
     tags {
         Name = "allow_all"
     }
+}
+
+resource "aws_key_pair" "plex-spot" {
+    key_name = "plex-spot"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD2M/nGn0AzXKYs7slmocrxvOLp503EWEyxhADWUerFTtXQRcExWei2wxCyEbwOcuQuE/MihAATaHtfZO8Ev9rH03j8xdg8iQZt2nNjhL+MVNcsniHGjn7keZFAFAQGSrK6heBtPaoX4DQjjUxQcbp8suby22hNuGDpTslGlRyC+Zdo2xmpNC10NeZYGyLXcd6Dje+yBG2/6PcAjR4ORengLgu1ouHA0DDs45ulx/4TwGIblE0bzVjBS2KLX7PLxWOzZ99GPKnCaY024CKHXka0C00zfQCXqOoHEDZu4GaUuLBdOW6ewSG08Sajf4XauNVfhGSqTTXSMf6L5LaEaMJP shakefu@gmail.com"
 }
