4566269237faf65ce205f8f5db2b3f3f3b083c1e
Jacob Alheid <shakefu@gmail.com>
2015-11-23 21:26:20 -0800

    Foolin'.

--- spot.tf
+++ spot.tf
@@ -1,4 +1,39 @@
-resource "aws_instance" "example" {
+# Set up a single spot instance
+resource "aws_spot_instance_request" "plex-spot" {
     ami = "ami-73bed013"
-    instance_type = "t2.micro"
+    availability_zone = "us-west-1b"
+    instance_type = "m2.xlarge"
+    security_groups = [ "${aws_security_group.allow_all.id}" ]
+    key_name = "plex-spot"
+
+    # user_data = "${file("plex.yml")}"
+    # Spot settings
+    spot_price = "0.04"
+    spot_type = "one-time"
+
+    tags {
+        Name = "plex-spot"
+    }
+
+    root_block_device = {
+        volume_type = "gp2"
+        volume_size = "20"
+    }
+}
+
+
+resource "aws_security_group" "allow_all" {
+    name = "allow_all"
+    description = "Allow all inbound traffic"
+
+    ingress {
+        from_port = 0
+        to_port = 65535
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags {
+        Name = "allow_all"
+    }
 }
