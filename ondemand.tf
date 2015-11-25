042110e65564f6bfe8ea9d99bca6c61310798739
Jacob Alheid <shakefu@gmail.com>
2015-11-24 19:06:23 -0800

    Fixy.

--- ondemand.tf
+++ ondemand.tf
@@ -18,25 +18,3 @@ resource "aws_instance" "plex" {
     }
 }
 
-resource "aws_security_group" "allow_all" {
-    name = "allow_all"
-    description = "Allow all inbound traffic"
-
-    ingress {
-        from_port = 0
-        to_port = 65535
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    tags {
-        Name = "allow_all"
-    }
-}
